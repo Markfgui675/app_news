67480b5ce3de81b644b86c6ec79a5524bca0d214
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2023-05-10 18:06:55 -0300

    Chore:

--- lib/View/Home.dart
+++ lib/View/Home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../api.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,11 +12,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  bool _naoTemResultado = false;
+
+  Future<Map<String, dynamic>> pesquisar() async {
+
+    var url = Uri.parse('https://newsapi.org/v2/everything?q=bitcoin&apiKey=800b8ed63a5d4913b2f5d16ca469ca0f');
+    var response = await http.get(url);
+
+    print('Data atual: ${DateTime.now()}');
+    print(response.statusCode);
+    if(response.statusCode == 200){
+      dynamic result = json.decode(response.body);
+      print(result);
+      return json.decode(response.body);
+    }
+    else {
+      throw Exception('Erro ao carregar dados do servidor.');
+    }
+
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    //pesquisar();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home'),
+      appBar: AppBar(
+        title: Text('Notícias'),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: pesquisar(),
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Center(
+              child: Text('Erro ao carregar notícias'),
+            );
+          }
+
+          if(snapshot.hasData){
+
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index){
+                  return ListTile(
+
+                    title: Text(snapshot.data!['articles'][index]['title']),
+
+                  );
+                }
+            );
+
+          }
+
+          return Center(
+            child: CircularProgressIndicator()
+          );
+        },
       ),
     );
   }
---
+++ lib/api.dart
@@ -0,0 +1,4 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+String api_key = '800b8ed63a5d4913b2f5d16ca469ca0f';
